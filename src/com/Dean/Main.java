ee61d6ab9bb9248ed3da0f63285bbab54abc6e75
Dean <singh.deepak007@hotmail.com>
2018-11-19 14:00:31 +0530

    test

---
+++ src/com/Dean/Main.java
@@ -0,0 +1,14 @@
+package com.Dean;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        for(int i=0;i<6;i++)
+    System.out.println("MY NAME IS DEAN");
+
+
+
+
+    }
+}
